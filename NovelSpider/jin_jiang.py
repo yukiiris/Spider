c7fcc751abb008e56810a189318a687fc1d06ae8
moltres <25852469@qq.com>
2017-08-24 23:22:05 +0800

    基本完成

--- NovelSpider/jin_jiang.py
+++ NovelSpider/jin_jiang.py
@@ -5,21 +5,21 @@ from bs4 import BeautifulSoup
 import re
 from selenium import webdriver
 import time
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
 
 class JinJiang(object):
 
     def __init__(self, urls):
         self.path = "D:\\code\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs"
+        self.opera = "D:\\code\\geckodriver.exe"
         self.phantomjs = webdriver.PhantomJS(self.path)
         self.downloader = page_downloader.Downloader()
         self.parser = page_parser.Parser()
         self.outputer = page_outputer.Outputer()
         self.urls = urls
         self.google = "C:\\Users\\asus\\Downloads\\chromedriver_win32 (1)\\chromedriver"
-        self.driver = webdriver.Chrome(self.google)
-        self.driver.set_page_load_timeout(20)
-        self.driver.set_script_timeout(20)
+        self.driver = webdriver.Firefox(executable_path=self.opera)
         self.login()
         print("初始化成功")
 
@@ -40,7 +40,6 @@ class JinJiang(object):
 
         self.get_chapter(url)
         page = self.driver.page_source
-        print(page)
         soup = BeautifulSoup(page, "lxml")
         text = soup.select(".noveltext")[0]
         unvip_regex = "<div style=\"clear:both;\"></div>([\s\S\D]*?)<div"
@@ -90,10 +89,10 @@ class JinJiang(object):
         self.driver.find_element_by_id("loginpassword").clear()
         self.driver.find_element_by_id("loginname").send_keys("")
         self.driver.find_element_by_id("loginpassword").send_keys("")
-        if self.driver.find_element_by_id("login_auth_num") is not None:
-            time.sleep(10)
-        else:
-            time.sleep(1)
+        # if self.driver.find_element_by_id("login_auth_num") is not None:
+        #     time.sleep(10)
+        # else:
+        #     time.sleep(1)
         self.driver.find_element_by_class_name("loginSubmit").click()
 
     def get_page_by_cookie(self, url):
@@ -126,14 +125,18 @@ class JinJiang(object):
                 self.driver.get(url)
             except:
                 try:
-                    if self.driver.find_element_by_class_name("noveltext") is not None:
+                    if self.driver.find_element_by_id("favorite_3") is not None:
                         isLoad = True
                 except:
                     pass
             else:
                 isLoad = True
 
+
+
     def start(self):
+        self.driver.set_page_load_timeout(8)
+        self.driver.set_script_timeout(8)
         for url in self.urls:
             self.url = url
             self.get(url)
@@ -144,11 +147,14 @@ class JinJiang(object):
             t_urls = self.get_urls()
             print(t_urls)
             novel = []
-            self.driver.set_page_load_timeout(2)
-            self.driver.set_script_timeout(2)
+            self.driver.set_page_load_timeout(3)
+            self.driver.set_script_timeout(3)
             for t_url in t_urls:
                 novel.append(self.on_chapter(t_url))
             self.outputer.jin_jiang_outer(attributes, novel)
+        self.driver.quit()
 
 
-JinJiang(["http://www.jjwxc.net/onebook.php?novelid=3185717"]).start()
+# for url in ['http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=1', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=2', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=3', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=4', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=5', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=6', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=7', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=8', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=9', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=10', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=11', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=12', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=13', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=14', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=15', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=16', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=17', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=18', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=19', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=20', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=21', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=22', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=23', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=24', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=25', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=26', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=27', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=28', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=29', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=30', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=31', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=32', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=33', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=34', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=35', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=36', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=37', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=38', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=39', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=40', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=41', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=42', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=43', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=44']:
+#     get_chapter(url)
+JinJiang(["http://www.jjwxc.net/onebook.php?novelid=3047399"]).start()
--- NovelSpider/test.py
+++ NovelSpider/test.py
@@ -2,14 +2,32 @@ import page_downloader
 import page_parser
 import  page_outputer
 from bs4 import BeautifulSoup
-import re
-from selenium import webdriver
 import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome("C:\\Users\\asus\\Downloads\\chromedriver_win32 (1)\\chromedriver")
-driver.set_script_timeout(5)
-driver.set_page_load_timeout(5)
-try:
-    driver.get("http://www.jjwxc.net/onebook.php?novelid=3185717&chapterid=1")
-except:
-    print(driver.page_source)
+driver.set_script_timeout(2)
+driver.set_page_load_timeout(2)
+def get_chapter(url):
+    isLoad = False
+
+    while isLoad is False:
+        print(1)
+        try:
+            print(2)
+            driver.get(url)
+            # self.driver.execute_script("window.stop()")
+            # WebDriverWait(self.driver, 5, 0.5).until(
+            #     lambda x: x.find_element_by_class_name("noveltext").is_displayed())
+        except:
+            try:
+                driver.refresh()
+            except:
+                pass
+        else:
+            isLoad = True
+
+for url in ['http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=1', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=2', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=3', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=4', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=5', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=6', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=7', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=8', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=9', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=10', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=11', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=12', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=13', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=14', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=15', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=16', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=17', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=18', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=19', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=20', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=21', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=22', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=23', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=24', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=25', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=26', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=27', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=28', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=29', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=30', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=31', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=32', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=33', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=34', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=35', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=36', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=37', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=38', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=39', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=40', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=41', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=42', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=43', 'http://www.jjwxc.net/onebook.php?novelid=2696632&chapterid=44']:
+    get_chapter(url)
